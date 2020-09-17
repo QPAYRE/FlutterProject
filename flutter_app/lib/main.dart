1c9f51d026b694f5ea1059b5c321d09070e86c9d
kent <quentin.payre@epitech.eu>
2020-09-17 10:53:36 +0200

    TP 1

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Container(
+        color: Colors.green,
+        child: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.greenAccent,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                )
+              ],
+            )
+        ),
+      )
+
+    );
+  }
+}
